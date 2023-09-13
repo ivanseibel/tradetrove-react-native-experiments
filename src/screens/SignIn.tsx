a41314e051aefe62664d33bbbd773e08225a35bc
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-09-14 00:00:00 +0100

    Update text color and font in SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,8 +3,8 @@ import { Box, Text } from '@gluestack-ui/themed'
 export const SignIn = () => {
   return (
     <Box flex={1} justifyContent="center" alignItems="center" bg="$blue-light">
-      <Text color="$blue" fontWeight="$bold">
-        This is the Box
+      <Text color="$gray-1" fontFamily="$heading" fontSize={'$2xl'}>
+        marketspace
       </Text>
     </Box>
   )
