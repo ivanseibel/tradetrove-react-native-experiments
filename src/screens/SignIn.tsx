80ca8175e38705e9ad2c894000934880299147dc
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-23 20:06:17 +0000

    Update sign-in screen styling

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,9 +2,14 @@ import { Box, Text } from '@gluestack-ui/themed'
 
 export const SignIn = () => {
   return (
-    <Box flex={1} justifyContent="center" alignItems="center" bg="$blue-light">
-      <Text color="$gray-1" fontFamily="$heading" fontSize={'$2xl'}>
-        marketspace
+    <Box flex={1} justifyContent="center" alignItems="center" bg="$gray6">
+      <Text
+        color="$gray1"
+        fontFamily="$heading"
+        fontSize={'$2xl'}
+        lineHeight={'$2xl'}
+      >
+        Sign In
       </Text>
     </Box>
   )
