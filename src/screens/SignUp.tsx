edef6f101e82e2fcbeddbfcf746b343e0159ab98
Ivan Seibel <me@ivanseibel.dev>
2024-02-25 17:01:34 +0000

    Delete SignUp screen component

--- src/screens/SignUp.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export const SignUp = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Sign Up</Text>
-    </View>
-  )
-}
