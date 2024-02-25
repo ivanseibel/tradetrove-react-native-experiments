b9fa72b5392037a8f9904cbd28a018621a61f073
Ivan Seibel <me@ivanseibel.dev>
2024-02-25 12:22:09 +0000

    Add TextInput component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,47 @@
+import {
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+  TextInputProps,
+} from 'react-native'
+import * as SC from './styles'
+import { useState } from 'react'
+
+type ComponentProps = TextInputProps & {
+  errorMessage?: string
+}
+
+export const TextInput = ({
+  errorMessage = undefined,
+  onChange,
+  ...rest
+}: ComponentProps) => {
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+
+  const handleOnChange = (
+    e: NativeSyntheticEvent<TextInputChangeEventData>
+  ) => {
+    if (onChange) {
+      onChange(e)
+    }
+
+    console.log('e.nativeEvent.text', e.nativeEvent.text)
+    setIsFilled(!!e.nativeEvent.text)
+  }
+
+  return (
+    <SC.InputController>
+      <SC.TextInput
+        error={!!errorMessage}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        isFocused={isFocused}
+        isFilled={isFilled}
+        {...rest}
+        onChange={handleOnChange}
+      />
+
+      {!!errorMessage && <SC.InputErrorLabel>Invalid email</SC.InputErrorLabel>}
+    </SC.InputController>
+  )
+}
