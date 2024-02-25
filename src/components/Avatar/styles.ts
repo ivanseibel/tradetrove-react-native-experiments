db14c56ef1ac7c33d1bf4af59f24d99d73699cae
Ivan Seibel <me@ivanseibel.dev>
2024-02-25 17:43:06 +0000

    Add Avatar component styles

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,42 @@
+import { PencilSimple } from 'phosphor-react-native'
+import { TouchableOpacity, Image } from 'react-native'
+import styled from 'styled-components/native'
+
+export const MainContainer = styled(TouchableOpacity).attrs({
+  activeOpacity: 0.9,
+})`
+  width: 88px;
+  height: 88px;
+`
+
+export const AvatarContainer = styled.View`
+  width: 100%;
+  height: 100%;
+  border-radius: 44px;
+  border-color: ${({ theme }) => theme.colors.bluelight};
+  position: relative;
+`
+
+export const AvatarImage = styled(Image).attrs({
+  resizeMode: 'cover',
+})`
+  width: 100%;
+  height: 100%;
+`
+
+export const EditButton = styled.View`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.colors.bluelight};
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  right: -8px;
+`
+
+export const EditIcon = styled(PencilSimple).attrs({
+  size: 16,
+  color: '#fff',
+})``
