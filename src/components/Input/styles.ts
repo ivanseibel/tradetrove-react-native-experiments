6d73a06a6284ace9029105df4e5d60dddaa1fbde
Ivan Seibel <me@ivanseibel.dev>
2024-02-25 14:57:10 +0000

    Refactor Input component styles

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,9 @@
 import theme from '@theme/index'
+import { Eye, EyeClosed } from 'phosphor-react-native'
 import { TextInput as NativeTextInput } from 'react-native'
 import styled from 'styled-components/native'
 
-export type StyledInputProps = {
+type InputContainerProps = {
   error?: boolean
   isFocused?: boolean
   isFilled?: boolean
@@ -17,24 +18,51 @@ export const InputErrorLabel = styled.Text`
   color: ${theme.colors.redlight}
 `
 
+export const TextInputContainer = styled.View<InputContainerProps>`
+  width: 100%;
+  height: 45px;
+
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  padding-right: 16px;
+
+  background-color: ${theme.colors.gray7};
+  border-width: ${({ error, isFocused, isFilled }) =>
+    error || isFocused || isFilled ? '1px' : '0px'};
+    border-radius: 6px;
+    border-color: ${({ error, isFocused, isFilled }) =>
+      error
+        ? theme.colors.redlight
+        : isFocused || isFilled
+          ? theme.colors.gray3
+          : 'transparent'};
+`
+
 export const TextInput = styled(NativeTextInput).attrs({
   placeholderTextColor: theme.colors.gray4,
-})<StyledInputProps>`
-  height: 45px;
-  width: 100%;
+})`
+  height: 100%;
+  flex: 1;
 
-  padding: 12px 16px;
+  padding: 12px 0 12px 16px;
 
   align-items: center;
 
-  background-color: ${theme.colors.gray7};
   border-radius: 6px;
-  border-width: ${({ error, isFocused, isFilled }) =>
-    error || isFocused || isFilled ? '1px' : '0px'};
-  border-color: ${({ error, isFocused, isFilled }) =>
-    error
-      ? theme.colors.redlight
-      : isFocused || isFilled
-        ? theme.colors.gray3
-        : 'transparent'};
+
+  color: ${theme.colors.gray2};
+  font-size: ${theme.fontSize.md}px;
+  font-family: ${theme.fontFamily.regular};
+  line-height: ${theme.lineHeight.md}px;
 `
+
+export const EyeOpenIcon = styled(Eye).attrs({
+  size: 20,
+  color: theme.colors.gray3,
+})``
+
+export const EyeClosedIcon = styled(EyeClosed).attrs({
+  size: 20,
+  color: theme.colors.gray3,
+})``
