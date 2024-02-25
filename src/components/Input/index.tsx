cf8224f933fac47db89d5fb5097cc77557dd7e60
Ivan Seibel <me@ivanseibel.dev>
2024-02-25 14:57:05 +0000

    Add password visibility toggle to TextInput component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,21 +2,29 @@ import {
   NativeSyntheticEvent,
   TextInputChangeEventData,
   TextInputProps,
+  TouchableOpacity,
 } from 'react-native'
 import * as SC from './styles'
 import { useState } from 'react'
 
 type ComponentProps = TextInputProps & {
   errorMessage?: string
+  type?: 'text' | 'password'
 }
 
 export const TextInput = ({
   errorMessage = undefined,
   onChange,
+  type = 'text',
   ...rest
 }: ComponentProps) => {
   const [isFocused, setIsFocused] = useState(false)
   const [isFilled, setIsFilled] = useState(false)
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+
+  const handleTogglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible)
+  }
 
   const handleOnChange = (
     e: NativeSyntheticEvent<TextInputChangeEventData>
@@ -29,17 +37,31 @@ export const TextInput = ({
 
   return (
     <SC.InputController>
-      <SC.TextInput
+      {!!errorMessage && (
+        <SC.InputErrorLabel>{errorMessage}</SC.InputErrorLabel>
+      )}
+      <SC.TextInputContainer
         error={!!errorMessage}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
         isFocused={isFocused}
         isFilled={isFilled}
-        {...rest}
-        onChange={handleOnChange}
-      />
+      >
+        <SC.TextInput
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          secureTextEntry={type === 'password' && !isPasswordVisible}
+          {...rest}
+          onChange={handleOnChange}
+        />
 
-      {!!errorMessage && <SC.InputErrorLabel>Invalid email</SC.InputErrorLabel>}
+        {type === 'password' && (
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={handleTogglePasswordVisibility}
+          >
+            {isPasswordVisible ? <SC.EyeOpenIcon /> : <SC.EyeClosedIcon />}
+          </TouchableOpacity>
+        )}
+      </SC.TextInputContainer>
     </SC.InputController>
   )
 }
