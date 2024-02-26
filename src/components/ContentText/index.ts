df9753873cc0d330aadaa78a2736663bfd0498c8
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-26 19:35:07 +0000

    Update ContentText component to use 'dark' and 'light' instead of 'black' and 'gray' for the 'type' prop

--- src/components/ContentText/index.ts
+++ src/components/ContentText/index.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components/native'
 
 type TextProps = {
-  type: 'black' | 'gray'
+  type: 'dark' | 'light'
   centered?: boolean
 }
 
 export const ContentText = styled.Text<TextProps>`
   color: ${({ theme, type }) =>
-    type === 'black' ? theme.colors.gray2 : theme.colors.gray4};
+    type === 'dark' ? theme.colors.gray2 : theme.colors.gray4};
   font-size: ${({ theme }) => theme.fontSize.sm}px;
   line-height: ${({ theme }) => theme.lineHeight.md}px;
   font-family: ${({ theme }) => theme.fontFamily.regular};
