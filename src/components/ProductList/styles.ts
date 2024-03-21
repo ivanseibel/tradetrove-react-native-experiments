de87209718c8c2a35aee22e50ab9e32c2c45b025
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-21 16:45:36 +0000

    Update styles in ProductList component

--- src/components/ProductList/styles.ts
+++ src/components/ProductList/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native'
 
 export const Main = styled.View`
   flex: 1;
-  gap: 24px;
+  gap: 12px;
   width: 100%;
 `
 
@@ -17,4 +17,12 @@ export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fontFamily.regular};
   font-size: ${({ theme }) => theme.fontSize.sm}px;
   line-height: ${({ theme }) => theme.lineHeight.sm}px;
+`
+export const ProductsHeader = styled.View`
+  display: flex;
+  align-self: stretch;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 32px;
 `
