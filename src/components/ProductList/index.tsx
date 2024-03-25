69a86c2d789cf430cee1cf89ed7317cf19878d09
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-25 17:33:19 +0000

    Refactor ProductList component to use nested seller object

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -13,7 +13,11 @@ type ProductType = {
   image: string
   active?: boolean
   condition: 'new' | 'used'
-  seller_avatar: string
+  seller: {
+    id: string
+    name: string
+    avatar: string
+  }
 }
 
 type ComponentProps = {
@@ -25,13 +29,17 @@ const MY_ADS_FILTER_OPTIONS = ['All', 'Active', 'Inactive']
 const products: ProductType[] = [
   {
     id: '1',
-    name: 'Nike Air Max 2021 jfjfj alalkak badum',
+    name: 'Nike Air Max 2021',
     price: 150,
     active: true,
     image:
       'https://www.shuropody.com/cdn/shop/collections/Women_s_trainers.jpg?v=1647954823',
     condition: 'used',
-    seller_avatar: 'https://github.com/ivanseibel.png',
+    seller: {
+      id: '999',
+      name: 'Ivan Seibel',
+      avatar: 'https://github.com/ivanseibel.png',
+    },
   },
   {
     id: '2',
@@ -41,7 +49,11 @@ const products: ProductType[] = [
     image:
       'https://www.zdnet.com/a/img/resize/771843da7bfb3d3f37b0dca8da1bf9017b221f9a/2020/12/11/bb70ffc0-7d9a-4ed6-a29d-67e9044f2344/top.jpg?auto=webp&fit=crop&height=1200&width=1200',
     condition: 'new',
-    seller_avatar: 'https://github.com/ivanseibel.png',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '3',
@@ -49,7 +61,11 @@ const products: ProductType[] = [
     price: 50,
     image: 'https://coffeeclick.ie/wp-content/uploads/2021/09/French-press.png',
     condition: 'new',
-    seller_avatar: 'https://github.com/ivanseibel.png',
+    seller: {
+      id: '999',
+      name: 'Ivan Seibel',
+      avatar: 'https://github.com/ivanseibel.png',
+    },
   },
   {
     id: '4',
@@ -57,81 +73,127 @@ const products: ProductType[] = [
     price: 20,
     image: 'https://m.media-amazon.com/images/I/51C8GTKZ1DL.jpg',
     condition: 'used',
-    seller_avatar: 'https://github.com/ivanseibel.png',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '5',
-    name: 'Nike Air Max 2021',
+    name: 'Dr. Martens Boots',
     price: 150,
-    image: 'https://www.shuropody.com/cdn/shop/collections',
+    image:
+      'https://media.glamour.com/photos/5c786327c8f3282db5088766/4:3/w_2400,h_1800,c_limit/0301_drmarten_lede_social_River.jpg',
     condition: 'used',
-    seller_avatar: 'https://github.com/ivanseibel.png',
+    seller: {
+      id: '999',
+      name: 'Ivan Seibel',
+      avatar: 'https://github.com/ivanseibel.png',
+    },
   },
   {
     id: '6',
-    name: 'Macbook Pro 2020',
+    name: 'Tripod for Camera',
     price: 2500,
     image:
-      'https://www.zdnet.com/a/img/resize/771843da7bfb3d3f37b0dca8da1bf9017b221f9a/2020/12/11/bb70ffc0-7d9a-4ed6-a29d-67e9044f2344/top.jpg?auto=webp&fit=crop&height=1200&width=1200',
+      'https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_2.625,f_auto,h_214,q_auto,w_380/c_pad,h_214,w_380/R1438983-01?pgw=1',
     condition: 'new',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '7',
-    name: 'French Coffee Press',
+    name: 'Italian Coffee Press',
     price: 50,
-    image: 'https://coffeeclick.ie/wp-content/uploads/2021/09/French-press.png',
+    image:
+      'https://majestycoffee.com/cdn/shop/articles/moka_pot_1534553b-bf80-4b30-86fc-e1087b7cc9f5_800x.jpg?v=1684797291',
     condition: 'new',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '8',
-    name: 'iPhone Charger',
+    name: 'Bluetooth Speaker JBL',
     price: 20,
-    image: 'https://m.media-amazon.com/images/I/51C8GTKZ1DL.jpg',
+    image: 'https://image.smythstoys.com/original/desktop/212788.jpg',
     condition: 'used',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '9',
-    name: 'Nike Air Max 2021',
+    name: 'Adidas Superstar 2021',
     price: 150,
-    image: 'https://www.shuropody.com/cdn/shop/collections',
+    image:
+      'https://images.stockx.com/images/adidas-Superstar-Chinese-New-Year-2021-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&dpr=2&trim=color&updated_at=1621454856&q=60',
     condition: 'used',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '10',
-    name: 'Macbook Pro 2020',
+    name: 'DJI Drone Mini 2',
     price: 2500,
     image:
-      'https://www.zdnet.com/a/img/resize/771843da7bfb3d3f37b0dca8da1bf9017b221f9a/2020/12/11/bb70ffc0-7d9a-4ed6-a29d-67e9044f2344/top.jpg?auto=webp&fit=crop&height=1200&width=1200',
+      'https://www.dpreview.com/files/p/articles/6857732420/DJI_Mavic_Mini_2.jpeg',
     condition: 'new',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '11',
-    name: 'French Coffee Press',
+    name: 'Bicycle Helmet',
     price: 50,
-    image: 'https://coffeeclick.ie/wp-content/uploads/2021/09/French-press.png',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/ba/Bicycle_Helmet_0085.jpg',
     condition: 'new',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '12',
-    name: 'iPhone Charger',
+    name: 'Cricket Ball',
     price: 20,
-    image: 'https://m.media-amazon.com/images/I/51C8GTKZ1DL.jpg',
+    image:
+      'https://www.shutterstock.com/image-vector/cricket-ball-sports-equipment-realistic-600nw-468996683.jpg',
     condition: 'used',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
   {
     id: '13',
-    name: 'Nike Air Max 2021',
+    name: 'Pilot Sunglasses',
     price: 150,
-    image: 'https://www.shuropody.com/cdn/shop/collections',
+    image:
+      'https://media.tiffany.com/is/image/Tiffany/EcomItemL2/tiffany-tpilot-sunglasses-70455218_1029533_ED.jpg',
     condition: 'used',
-    seller_avatar: '',
+    seller: {
+      id: '998',
+      name: 'Ivan Seibel',
+      avatar: 'https://thispersondoesnotexist.com/',
+    },
   },
 ]
 
@@ -209,9 +271,9 @@ export const ProductList = ({ headerType }: ComponentProps) => {
             active={item.active}
             showSeller={headerType !== 'myAds'}
             seller={{
-              id: '999',
-              name: 'Ivan Seibel',
-              avatar: item.seller_avatar,
+              id: item.seller.id,
+              name: item.seller.name,
+              avatar: item.seller.avatar,
             }}
           />
         )}
