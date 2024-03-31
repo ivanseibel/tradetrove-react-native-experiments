9e69fe1c278c710bab150990687c1d7d36c459f3
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-31 12:32:27 +0100

    Add styles for ImageUploadComponent

---
+++ src/components/ImageUploadComponent/styles.ts
@@ -0,0 +1,46 @@
+// For ImageContainer gets the device dimensions to set the width and height
+import { Dimensions } from 'react-native'
+import styled from 'styled-components/native'
+
+const { width } = Dimensions.get('window')
+
+export const Main = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`
+
+export const ImageContainer = styled.View`
+  position: relative;
+  width: ${width / 3 - 20}px;
+  height: ${width / 3 - 20}px;  
+  border-radius: 6px;
+`
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  border-radius: 6px;
+`
+
+export const RemoveButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  background-color: ${({ theme }) => theme.colors.gray1};
+  padding: 4px;
+  border-radius: 50px;
+`
+
+export const Placeholder = styled.TouchableOpacity`
+  width: ${width / 3 - 20}px;
+  height: ${width / 3 - 20}px;
+  border-radius: 6px;
+  background-color: ${({ theme }) => theme.colors.gray5};
+  justify-content: center;
+  align-items: center;
+  /* border-width: 1px; */
+  /* border-color: ${({ theme }) => theme.colors.gray2}; */
+  /* border-style: dashed; */
+`
