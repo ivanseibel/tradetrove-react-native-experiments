920c2e6c92cd256156a7a551b758e593cb52ae6c
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-03-31 13:57:06 +0100

    Add multiline support to Input component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -7,6 +7,11 @@ type InputContainerProps = {
   error?: boolean
   isFocused?: boolean
   isFilled?: boolean
+  multiline?: boolean
+}
+
+type InputProps = {
+  multilineHeight?: number
 }
 
 export const InputController = styled.View`
@@ -20,7 +25,7 @@ export const InputErrorLabel = styled.Text`
 
 export const TextInputContainer = styled.View<InputContainerProps>`
   width: 100%;
-  height: 45px;
+  height: ${({ multiline }) => (multiline ? 'auto' : '45px')};
 
   flex-direction: row;
   align-items: center;
@@ -41,20 +46,20 @@ export const TextInputContainer = styled.View<InputContainerProps>`
 
 export const TextInput = styled(NativeTextInput).attrs({
   placeholderTextColor: theme.colors.gray4,
-})`
-  height: 100%;
-  flex: 1;
-
-  padding: 12px 0 12px 16px;
-
-  align-items: center;
-
-  border-radius: 6px;
-
+})<InputProps>`
+flex: 1;
+  padding: 12px 16px;
   color: ${theme.colors.gray2};
   font-size: ${theme.fontSize.md}px;
   font-family: ${theme.fontFamily.regular};
   line-height: ${theme.lineHeight.md}px;
+  
+  ${({ multiline, multilineHeight }) =>
+    multiline &&
+    `
+    height: ${multilineHeight}px;
+    text-align-vertical: top;
+  `}
 `
 
 export const EyeOpenIcon = styled(Eye).attrs({
