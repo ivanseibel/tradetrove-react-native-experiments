9b68e52fb2d4a540871d9e703c52b357ceabc958
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-04-03 00:14:49 +0100

    Add TextInput component styles

---
+++ src/components/Input/TextInput/styles.ts
@@ -0,0 +1,26 @@
+import theme from '@theme/index'
+import styled from 'styled-components/native'
+import { TextInput as NativeTextInput } from 'react-native'
+
+type InputProps = {
+  multilineHeight?: number
+}
+
+export const TextInput = styled(NativeTextInput).attrs({
+  placeholderTextColor: theme.colors.gray4,
+})<InputProps>`
+  flex: 1;
+  min-width: 200px;
+  padding: 12px 16px;
+  color: ${theme.colors.gray2};
+  font-size: ${theme.fontSize.md}px;
+  font-family: ${theme.fontFamily.regular};
+  line-height: ${theme.lineHeight.md}px;
+  
+  ${({ multiline }) =>
+    multiline &&
+    `
+    height: 160px;
+    text-align-vertical: top;
+  `}
+`
