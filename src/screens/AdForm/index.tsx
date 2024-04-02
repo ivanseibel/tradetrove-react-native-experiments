ba8207e5a9bfaf099dbb9a5a027bc9635089dca4
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-04-03 00:15:12 +0100

    Refactor AdForm component to include keyboard handling and price input

--- src/screens/AdForm/index.tsx
+++ src/screens/AdForm/index.tsx
@@ -1,53 +1,86 @@
 import { ImageUploadComponent } from '@components/ImageUploadComponent'
 import * as SC from './styles'
-import { View } from 'react-native'
+import {
+  View,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Platform,
+  ScrollView,
+  Keyboard,
+} from 'react-native'
 import { TextInput } from '@components/Input'
 import { RadioButtonGroup } from '@components/RadioButtonGroup'
+import { useNavigation } from '@react-navigation/native'
 
 const fakeProps = {
   operation: 'create',
 }
 
 export const AdForm = () => {
+  const navigation = useNavigation()
+
+  const handleBack = () => {
+    navigation.goBack()
+  }
+
   return (
-    <SC.Main>
-      <SC.Header>
-        <SC.NavigationButton>
-          <SC.BackIcon />
-        </SC.NavigationButton>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
+        style={{ flex: 1 }}
+      >
+        <ScrollView>
+          <SC.Main>
+            <SC.Header>
+              <SC.NavigationButton onPress={handleBack}>
+                <SC.BackIcon />
+              </SC.NavigationButton>
 
-        <SC.HeaderTitle>
-          {fakeProps.operation === 'create' ? 'Create Ad' : 'Edit Ad'}
-        </SC.HeaderTitle>
-      </SC.Header>
+              <SC.HeaderTitle>
+                {fakeProps.operation === 'create' ? 'Create Ad' : 'Edit Ad'}
+              </SC.HeaderTitle>
+            </SC.Header>
 
-      <SC.ImageContainer>
-        <SC.SectionTitle>Photos</SC.SectionTitle>
-        <SC.SectionDescription>
-          Add up to 3 photos of your item to make it more attractive
-        </SC.SectionDescription>
+            <SC.ImageContainer>
+              <SC.SectionTitle>Photos</SC.SectionTitle>
+              <SC.SectionDescription>
+                Add up to 3 photos of your item to make it more attractive
+              </SC.SectionDescription>
 
-        <View style={{ height: 10 }} />
+              <View style={{ height: 10 }} />
 
-        <ImageUploadComponent />
-      </SC.ImageContainer>
+              <ImageUploadComponent />
+            </SC.ImageContainer>
 
-      <View style={{ height: 20 }} />
+            <View style={{ height: 20 }} />
 
-      <SC.AboutContainer>
-        <SC.SectionTitle>About this product</SC.SectionTitle>
+            <SC.AboutContainer>
+              <SC.SectionTitle>About this product</SC.SectionTitle>
 
-        <TextInput placeholder="Title" />
-        <TextInput placeholder="Description" multiline numberOfLines={4} />
+              <TextInput placeholder="Title" />
+              <TextInput
+                placeholder="Description"
+                multiline
+                numberOfLines={4}
+              />
 
-        <RadioButtonGroup
-          options={[
-            { id: '1', label: 'New' },
-            { id: '2', label: 'Used' },
-          ]}
-          onSelect={(selectedId) => console.log(selectedId)}
-        />
-      </SC.AboutContainer>
-    </SC.Main>
+              <RadioButtonGroup
+                options={[
+                  { id: '1', label: 'New' },
+                  { id: '2', label: 'Used' },
+                ]}
+                onSelect={(selectedId) => console.log(selectedId)}
+              />
+
+              <SC.PriceContainer>
+                <SC.Currency>EUR</SC.Currency>
+                <TextInput placeholder="Price" />
+              </SC.PriceContainer>
+            </SC.AboutContainer>
+          </SC.Main>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   )
 }
