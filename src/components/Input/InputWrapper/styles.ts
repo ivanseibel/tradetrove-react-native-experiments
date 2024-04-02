9b2716bca0a2da38a4c429526416303e1b0896bd
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-04-03 00:14:28 +0100

    Add styles for InputWrapper component

---
+++ src/components/Input/InputWrapper/styles.ts
@@ -0,0 +1,44 @@
+import theme from '@theme/index'
+import { Eye, EyeClosed } from 'phosphor-react-native'
+import { TextInput as NativeTextInput } from 'react-native'
+import styled from 'styled-components/native'
+
+type InputContainerProps = {
+  error?: boolean
+  isFocused?: boolean
+  isFilled?: boolean
+  multiline?: boolean
+}
+
+// type InputProps = {
+//   multilineHeight?: number
+// }
+
+export const InputController = styled.View`
+  width: 100%;
+  gap: 4px;
+`
+
+export const InputErrorLabel = styled.Text`
+  color: ${theme.colors.redlight}
+`
+
+export const TextInputContainer = styled.View<InputContainerProps>`
+   width: 100%;
+
+   flex-direction: row;
+   align-items: center;
+   gap: 8px;
+   padding-right: 16px;
+
+   background-color: ${theme.colors.gray7};
+   border-width: ${({ error, isFocused, isFilled }) =>
+     error || isFocused || isFilled ? '1px' : '0px'};
+     border-radius: 6px;
+     border-color: ${({ error, isFocused, isFilled }) =>
+       error
+         ? theme.colors.redlight
+         : isFocused || isFilled
+            ? theme.colors.gray3
+            : 'transparent'};
+ `
