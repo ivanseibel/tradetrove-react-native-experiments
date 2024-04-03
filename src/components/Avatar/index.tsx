47a18c8ad52936d5f53540ff929e2f90bb9238ed
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-04-03 17:45:18 +0100

    Add image picker functionality to Avatar component

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,12 +1,27 @@
 import { ImageProps } from 'react-native'
 import * as SC from './styles'
 import DefaultPhoto from '@assets/default-photo.png'
+import * as ImagePicker from 'expo-image-picker'
 
-type ComponentProps = ImageProps
+type ComponentProps = ImageProps & {
+  setAvatar: (avatar: string) => void
+}
 
-export const Avatar = ({ source, ...rest }: ComponentProps) => {
+export const Avatar = ({ setAvatar, source, ...rest }: ComponentProps) => {
+  const handleSelectImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      allowsMultipleSelection: false,
+    })
+
+    if (!result.canceled) {
+      setAvatar(result.assets[0].uri)
+    }
+  }
+
   return (
-    <SC.MainContainer>
+    <SC.MainContainer onPress={handleSelectImage}>
       <SC.AvatarContainer>
         <SC.AvatarImage source={source ? source : DefaultPhoto} {...rest} />
         <SC.EditButton>
