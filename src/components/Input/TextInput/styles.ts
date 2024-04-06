f706f3e6a584414c5f77b522ddf4af1b75c99148
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-04-06 18:25:23 +0100

    Update padding in TextInput component styles

--- src/components/Input/TextInput/styles.ts
+++ src/components/Input/TextInput/styles.ts
@@ -11,7 +11,7 @@ export const TextInput = styled(NativeTextInput).attrs({
 })<InputProps>`
   flex: 1;
   min-width: 200px;
-  padding: 12px 16px;
+  padding: 12px 0;
   color: ${theme.colors.gray2};
   font-size: ${theme.fontSize.md}px;
   font-family: ${theme.fontFamily.regular};
