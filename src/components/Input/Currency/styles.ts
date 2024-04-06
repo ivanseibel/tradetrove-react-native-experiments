8ccf0fa0fe10055534c0199280f08c72b850dc44
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-04-06 18:24:59 +0100

    Add Currency component styles for TextInput

---
+++ src/components/Input/Currency/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+export const Currency = styled.Text`
+  color: ${({ theme }) => theme.colors.gray1};
+  font-family: ${({ theme }) => theme.fontFamily.regular};
+  font-size: ${({ theme }) => theme.fontSize.md}px;
+  line-height: 20.8px;
+  
+`
